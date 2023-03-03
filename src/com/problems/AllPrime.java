157997cbac03a4835c12184531885ef656dd36dd
saylee21 <sayleebharsakle@gmail.com>
2023-03-03 12:15:03 +0530

    fourteenth commit

---
+++ src/com/problems/AllPrime.java
@@ -0,0 +1,18 @@
+package com.problems;
+
+import java.util.Scanner;
+
+public class AllPrime {
+    public static void main(String[] args){
+        Scanner scn= new Scanner(System.in);
+        int low= scn.nextInt();
+        int high=scn.nextInt();
+
+        for(int n=low; n<=high; n++){
+            int count=0;
+            for(int div=2; div*div<=n; div++){
+                
+            }
+        }
+    }
+}
