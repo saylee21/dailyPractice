fd51b4a46fc1252f74105f66cb34a304db1d2ecb
saylee21 <sayleebharsakle@gmail.com>
2023-03-03 12:17:40 +0530

    fiftheenth commit

--- src/com/problems/AllPrime.java
+++ src/com/problems/AllPrime.java
@@ -11,7 +11,13 @@ public class AllPrime {
         for(int n=low; n<=high; n++){
             int count=0;
             for(int div=2; div*div<=n; div++){
-                
+                if(n%div==0){
+                    count++;
+                    break;
+                }
+                if(count==0){
+                    
+                }
             }
         }
     }
